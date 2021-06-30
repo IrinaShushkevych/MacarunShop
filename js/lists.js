2f3966ce256938470380bb6c35f3c22ed5758019
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-06-30 16:33:33 +0300

    list.js last

--- js/lists.js
+++ js/lists.js
@@ -1,17 +1,12 @@
 (() => {
   const btn = document.querySelectorAll('[data-button]');
 
-
-  for (var i=0; i<btn.length; i++) {
-     btn[i].addEventListener('click', function toggleModal(e) {
-          const btn = e.target;
-          const idx = btn.getAttribute('data-number')
-	  const elem = document.querySelectorAll('[data-number="'+idx+'"]')
-	  
-          for (var j = 0; j < elem.length; j++)
-	     elem[j].classList.toggle('is-open')
-	
-	})
-  }
-  
+  btn.forEach(   (b)=>{
+    b.addEventListener('click', function toggleModal(e) {
+	const btn = e.target;
+	const idx = btn.getAttribute('data-number')
+	const elem = document.querySelectorAll('[data-number="'+idx+'"]')
+	elem.forEach((e) => { e.classList.toggle('is-open')});
+    })
+  })
 })();
