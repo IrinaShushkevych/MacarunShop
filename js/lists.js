5c8cf12dcceceef4440eb4cf90bc36f6dba57669
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-06-30 16:28:18 +0300

    list.js

--- js/lists.js
+++ js/lists.js
@@ -1,26 +1,17 @@
 (() => {
   const btn = document.querySelectorAll('[data-button]');
-  var idx;
-  var block = new Object();
-  var iconu = new Object();
-  var icond = new Object();
-  var button = new Object();
 
 
   for (var i=0; i<btn.length; i++) {
      btn[i].addEventListener('click', function toggleModal(e) {
           const btn = e.target;
           const idx = btn.getAttribute('data-number')
-	  const block = document.querySelector('[data-block'+idx+']')
-	  const iconu = document.querySelector('[data-icon-up'+idx+']')
-	  const icond = document.querySelector('[data-icon-down'+idx+']')
-
-	  block.classList.toggle('is-open')
-	  iconu.classList.toggle('is-open')
-	  icond.classList.toggle('is-open')
+	  const elem = document.querySelectorAll('[data-number="'+idx+'"]')
+	  
+          for (var j = 0; j < elem.length; j++)
+	     elem[j].classList.toggle('is-open')
+	
 	})
   }
- 
-
   
 })();
