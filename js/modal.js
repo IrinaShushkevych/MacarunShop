c796e1fcd39892ca7fe46eedf904bc64d7e0a5fe
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-07-28 14:15:38 +0300

    Корпоративные подарки, модальные формы

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,38 @@
-;(() => {
+(() => {
   const modalButton = document.querySelectorAll('[data-modal-button-basket]')
   const modal = document.querySelector('[data-modal-basket]')
   const backdrop = document.querySelector('[data-backdrop]')
 
-  console.log(modalButton)
+  const modalButton1 = document.querySelectorAll('[data-modal-button-catalog]')
+  const modal1 = document.querySelector('[data-modal-catalog]')
+
+  const modalButton2 = document.querySelectorAll(
+    '[data-modal-button-catalog-all]',
+  )
+  const modal2 = document.querySelector('[data-modal-catalog-all]')
+
+  console.log(modalButton1)
 
   modalButton.forEach((el) => {
     console.log('element', el)
     el.addEventListener('click', () => {
       modal.classList.toggle('is-open')
+      backdrop.classList.toggle('is-open')
+    })
+  })
+
+  modalButton1.forEach((el) => {
+    console.log('element', el)
+    el.addEventListener('click', () => {
+      modal1.classList.toggle('is-open')
+      backdrop.classList.toggle('is-open')
+    })
+  })
+
+  modalButton2.forEach((el) => {
+    console.log('element', el)
+    el.addEventListener('click', () => {
+      modal2.classList.toggle('is-open')
       backdrop.classList.toggle('is-open')
     })
   })
