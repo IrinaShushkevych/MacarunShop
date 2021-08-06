657f6fc37f15e5c59de3a67ff0a6f33cd22d4f8b
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-08-06 15:38:05 +0300

    printed

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
-(() => {
+;(() => {
   const modalButton = document.querySelectorAll('[data-modal-button-basket]')
+
   const modal = document.querySelector('[data-modal-basket]')
   const backdrop = document.querySelector('[data-backdrop]')
 
@@ -11,6 +12,14 @@
   )
   const modal2 = document.querySelector('[data-modal-catalog-all]')
 
+  const modalButton3 = document.querySelectorAll(
+    '[data-modal-button-assembled-order]',
+  )
+  const modal3 = document.querySelector('[data-modal-order-assemble]')
+
+  const modalButton4 = document.querySelectorAll(
+    '[data-modal-button-basket-order]',
+  )
   console.log(modalButton1)
 
   modalButton.forEach((el) => {
@@ -34,6 +43,23 @@
     el.addEventListener('click', () => {
       modal2.classList.toggle('is-open')
       backdrop.classList.toggle('is-open')
+    })
+  })
+
+  modalButton3.forEach((el) => {
+    console.log('element', el)
+    el.addEventListener('click', () => {
+      modal3.classList.toggle('is-open')
+      backdrop.classList.toggle('is-open')
+    })
+  })
+
+  modalButton4.forEach((el) => {
+    console.log('element', el)
+    el.addEventListener('click', () => {
+      modal.classList.toggle('is-open')
+      modal3.classList.toggle('is-open')
+      // backdrop.classList.toggle('is-open')
     })
   })
 })()
